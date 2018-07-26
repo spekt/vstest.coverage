67b9b3c606a50d991b5b8095226c1afc73b4f758
Shreyas R <shrer@microsoft.com>
2018-07-26 10:53:42 +0530

    Added lcov generator (#24)
    * Generation of lcov file format
    
    * Test verified
    
    * Remove LcovGenerator projects
    
    * Remove LcovGenerator projects 2
    
    * Remove LcovGenerator projects 3
    
    * Move lcov generator project to src dir
    
    * Updated LcovGeneratorTests.csproj to latest target
    
    * fixed review comments
    
    * added wildcard for runnning tests
    
    * Update vstest.coverage.sln
    
    * fixed the yml files for the ci

---
+++ src/LcovGenerator/LcovModel.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Spekt.Vstest.Coverage.LcovGenerator
+{
+    public class LcovModel
+    {
+        public Dictionary<string, Dictionary<int, int>> Data;
+
+        public LcovModel()
+        {
+
+        }
+
+        public LcovModel(Dictionary<string, Dictionary<int, int>> lcovModelData)
+        {
+          this.Data = lcovModelData;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder lcovContents = new StringBuilder();
+
+            foreach (var sourceFile in this.Data)
+            {
+                lcovContents.AppendLine($"SF:{sourceFile.Key}");
+
+                foreach(var range in sourceFile.Value)
+                {
+                    lcovContents.AppendLine($"DA:{range.Key},{range.Value}");
+                }
+
+                lcovContents.AppendLine("end_of_record");
+            }
+
+            return lcovContents.ToString();
+        }
+    }
+
+    public class Generator
+    {
+        public LcovModel ModelConverter(CodeCoverageDataModel codeCoverageDataModel)
+        {
+            Dictionary<string, Dictionary<int, int>> lcovModelData = new Dictionary<string, Dictionary<int, int>>();
+
+            foreach (var range in codeCoverageDataModel.Ranges)
+            {
+                var source = codeCoverageDataModel.Sources[range.SourceFileId];
+
+
+                if (!lcovModelData.ContainsKey(source))
+                {
+                    lcovModelData.Add(source, new Dictionary<int, int>());
+                }
+
+                for (int i = range.StartLine; i <= range.EndLine; i++)
+                {
+                    if (range.Covered)
+                    {
+                        lcovModelData[source][i] = lcovModelData[source].ContainsKey(i) ? lcovModelData[source][i] + 1 : 1;
+                    }
+                    else
+                    {
+                        lcovModelData[source][i] = lcovModelData[source].ContainsKey(i) ? lcovModelData[source][i] : 0;
+                    }
+                }
+            }
+
+            return new LcovModel(lcovModelData);
+        }
+    }
+}
---
+++ src/LcovGeneratorTests/LcovGeneratorTests.cs
@@ -0,0 +1,86 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spekt.Vstest.Coverage;
+using Spekt.Vstest.Coverage.LcovGenerator;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LcovGeneratorTests
+{
+    [TestClass]
+    public class LcovGeneratorTests
+    {
+
+        public LcovGeneratorTests()
+        {
+            LcovModel lcovModel = new LcovModel();
+        }
+
+        [TestMethod]
+        public void GeneratorShouldReturnCorrectDictionary()
+        {
+            CodeCoverageDataModel codeCoverageDataModel = new CodeCoverageDataModel();
+            Generator generator = new Generator();
+            codeCoverageDataModel.Sources.Add("s1.cs");
+            codeCoverageDataModel.Sources.Add("s2.cs");
+            Range range1 = new Range(5, 6, true, 0);
+            Range range2 = new Range(9, 12, true, 1);
+            Range range3 = new Range(13, 13, false, 0);
+
+            codeCoverageDataModel.Ranges.Add(range1);
+            codeCoverageDataModel.Ranges.Add(range1);
+            codeCoverageDataModel.Ranges.Add(range2);
+            codeCoverageDataModel.Ranges.Add(range3);
+
+            Dictionary<string, Dictionary<int, int>> expecedLcovModelData = new Dictionary<string, Dictionary<int, int>>();
+            expecedLcovModelData.Add("s1.cs", new Dictionary<int, int> { { 5, 2 }, { 6, 2 }, { 13, 0 } });
+            expecedLcovModelData.Add("s2.cs", new Dictionary<int, int> { { 9, 1 }, { 10, 1 }, { 11, 1 }, { 12, 1 } });
+
+            var actualLcovModelData = generator.ModelConverter(codeCoverageDataModel);
+
+            foreach (var modelData in expecedLcovModelData)
+            {
+                CollectionAssert.AreEqual(expecedLcovModelData[modelData.Key], actualLcovModelData.Data[modelData.Key]);
+            }
+        }
+
+        [TestMethod]
+        public void LcovModelToStringShouldWriteToFile()
+        {
+            CodeCoverageDataModel codeCoverageDataModel = new CodeCoverageDataModel();
+            Generator generator = new Generator();
+
+            codeCoverageDataModel.Sources.Add("s1.cs");
+            codeCoverageDataModel.Sources.Add("s2.cs");
+
+            Range range1 = new Range(5, 6, true, 0);
+            Range range2 = new Range(9, 12, true, 1);
+            Range range3 = new Range(13, 13, false, 0);
+
+            codeCoverageDataModel.Ranges.Add(range1);
+            codeCoverageDataModel.Ranges.Add(range1);
+            codeCoverageDataModel.Ranges.Add(range2);
+            codeCoverageDataModel.Ranges.Add(range3);
+
+            var actualLcovModelData = generator.ModelConverter(codeCoverageDataModel);
+
+            var actualText = actualLcovModelData.ToString();
+            Console.WriteLine(actualText);
+
+            string expectedText = "SF:s1.cs" + Environment.NewLine +
+                "DA:5,2" + Environment.NewLine +
+                "DA:6,2" + Environment.NewLine +
+                "DA:13,0" + Environment.NewLine +
+                "end_of_record" + Environment.NewLine +
+                "SF:s2.cs" + Environment.NewLine +
+                "DA:9,1" + Environment.NewLine +
+                "DA:10,1" + Environment.NewLine +
+                "DA:11,1" + Environment.NewLine +
+                "DA:12,1" + Environment.NewLine +
+                "end_of_record" + Environment.NewLine;
+
+            Assert.AreEqual(actualText, expectedText);
+            System.Console.WriteLine(expectedText);
+        }
+    }
+}
